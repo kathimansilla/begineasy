b1fc75f8cfa1062fb09355f567e84b4c4b263f25
kathimansilla <mansillakatherina@gmail.com>
2023-08-15 13:23:09 +0200

    add event onClick in brand

--- src/components/App.js
+++ src/components/App.js
@@ -5,19 +5,26 @@ function App() {
   const [isLoading, setIsLoading] = useState(false);
   const [transitionColor, setTransitionColor] = useState(false);
 
+  // useEffect
   useEffect(() => {
     setTimeout(() => {
       setIsLoading(true);
     }, 1000);
   }, []);
 
+  // functions
   setInterval(() => {
     setTransitionColor(transitionColor ? false : true);
   }, 7000);
 
+  // handle functions
+  const handleClickBrand = (ev) => {
+    console.log('hice click en la marca');
+  };
+
   return (
     <div className={`container ${transitionColor ? 'transitionColor' : ''}`}>
-      <h1 className={`title ${isLoading ? 'start' : ''} ${transitionColor ? 'transitionColor' : ''}`}>BeginEasy</h1>
+      <h1 onClick={handleClickBrand} className={`title ${isLoading ? 'start' : ''} ${transitionColor ? 'transitionColor' : ''}`}>BeginEasy</h1>
     </div>
   );
 }
