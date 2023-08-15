e1618e3190bae76b7430ac1a74fedb83ca2cc49d
kathimansilla <mansillakatherina@gmail.com>
2023-08-15 13:05:49 +0200

    refactor code

--- src/components/App.js
+++ src/components/App.js
@@ -3,50 +3,23 @@ import '../styles/App.scss';
 
 function App() {
   const [isLoading, setIsLoading] = useState(false);
-  const [brand, setBrand] = useState('BeginEasy');
   const [transitionColor, setTransitionColor] = useState(false);
 
   useEffect(() => {
     setTimeout(() => {
-      setBrand('BeginEasy');
       setIsLoading(true);
     }, 1000);
   }, []);
 
   setInterval(() => {
     setTransitionColor(transitionColor ? false : true);
-  }, 10000);
+  }, 7000);
 
   return (
-    <div className={`container ${isLoading ? 'start' : ''} ${transitionColor ? 'transitionColor' : ''}`}>
-      <h1 className={`title ${isLoading ? 'start' : ''} ${transitionColor ? 'transitionColor' : ''}`}>{brand}</h1>
+    <div className={`container ${transitionColor ? 'transitionColor' : ''}`}>
+      <h1 className={`title ${isLoading ? 'start' : ''} ${transitionColor ? 'transitionColor' : ''}`}>BeginEasy</h1>
     </div>
   );
 }
 
 export default App;
-
-/*import '../styles/App.scss';
-import React from 'react';
-import { useState, useEffect } from 'react';
-
-function App() {
-  const [isLoading, setIsLoading] = useState(false);
-  const [brand, setBrand] = useState('BE');
-  
-  useEffect(() => {
-    setTimeout(()=>{
-      setBrand('BeginEasy');
-      setIsLoading(true);
-    }, 500);
-  }, []);
-
-  return (
-    <div className="container">
-      <h1 className={`title ${isLoading ? 'start' : ''}`}>{brand}</h1>
-    </div>
-  );
-}
-
-export default App;
-*/
